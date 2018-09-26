e4d2b79bc0347ae4b3c3c0ead43c01f4bc06dd1d
Bryce Hennen <ecyrbnenneh@gmail.com>
2018-09-26 14:52:46 -0500

    Add code to open test file and tokenize lines

--- Rover/autonomous/SearchAlgorithm/Tests/t_SearchAlgorithm.cpp
+++ Rover/autonomous/SearchAlgorithm/Tests/t_SearchAlgorithm.cpp
@@ -8,8 +8,51 @@
 
 #include "../SearchAlgorithm.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <regex>
+
+std::vector<std::string> tokenize(const std::string str, std::regex delim)
+{
+	std::sregex_token_iterator iter(str.begin(), str.end(), delim, -1);
+	std::sregex_token_iterator end;
+
+	std::vector<std::string> result(iter, end);
+
+	return result;
+}
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello World!\n";
+	std::string line;
+	std::ifstream myfile("../../Rover/autonomous/SearchAlgorithm/Tests/PathfindingTests/test1.txt"); 
+	
+	if (!myfile.is_open())
+	{
+		std::cout << "Unable to open file";
+	}
+	else
+	{
+		std::regex reg("\\s*,\\s*"); //Regex matches any whitespace, 1 comma, any whitespace
+		
+									 //Get first line and set dimensions of map
+		getline(myfile, line);
+		std::vector<std::string> tokens = tokenize(line, reg);
+		std::vector<std::string>::iterator it = tokens.begin();
+
+		int x = std::stoi(it[0]); //convert string to int
+		int y = std::stoi(it[1]); 
+		
+
+		/*std::cout << "Map input:\n";
+		while (getline(myfile, line))
+		{		
+			std::cout << line << '\n';
+
+		}*/
+		myfile.close();
+	}
+
+	return 0;
 }
