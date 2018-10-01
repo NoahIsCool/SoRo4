db80d3f272597104bddd550a5983863a6f878048
sir-kellsworth <kckswimmer@gmail.com>
2018-09-30 20:42:17 -0500

    moved video server ports to soro_global.h

--- Soro4/core/soro_global.h
+++ Soro4/core/soro_global.h
@@ -1,4 +1,14 @@
-#ifndef SORO_GLOBAL_H
+﻿#ifndef SORO_GLOBAL_H
 #define SORO_GLOBAL_H
+
+namespace videoStreamer{
+
+const int CONTROL_PORT          = 6969;
+const int CONTROL_CLIENT_PORT	= 6970;
+const int HEARTBEAT_PORT        = 6971;
+const int HEARTBEAT_CLIENT_PORT = 6972;
+const int CAMERA_PORT_BASE      = 7000;
+
+};
 
 #endif // SORO_GLOBAL_H
