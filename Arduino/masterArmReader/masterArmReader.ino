950a333646e753eac52a38a9f1fc8dfb0a439bef
ZacharySasser <43420343+ZacharySasser@users.noreply.github.com>
2018-10-03 18:53:29 -0500

    Update masterArmReader.ino

--- Arduino/masterArmReader/masterArmReader.ino
+++ Arduino/masterArmReader/masterArmReader.ino
@@ -35,24 +35,37 @@ void setup() {
 }
 
 void loop() {
-  sendData(); //function is seperate so we can expand this program if needed
+  
+  //this doesn't have to be in a seperate function I did this in case we want to add stuff not taking data later.
+  sendData(); 
 }
 
 void sendData(){
   //data is used as a "packet" for when we send it over serial (don't worry about the floating point math the int typecast automatically converts it after but it will throw a warning oh well)
+  //0.2490234375 is 255/1024 to convert the units of the potentiometer to fit in a byte
+  //On the other side you must multiply by 270/255 (1.0588235294) to convert it to degrees and 0.0184799567 to convert to radians (don't use radians you weirdo thats for math not humans)
   int data[11] = {-127, ID, (int)analogRead(joints[0])*0.2490234375,(int)analogRead(joints[1])*0.2490234375,(int)analogRead(joints[2])*0.2490234375,(int)analogRead(joints[3])*0.2490234375,0,0,0,0};
 
 
   //Figure out if the buttons are being pressed or not
-  for(int i = 4; i<11; i++){
+  for(int i = 0; i<4; i++){
+    
+    //Set the digital port i to pinmode of input so we can read data from it. (we don't ever need to set it to output I just did this for security)
     pinMode(buttons[i], INPUT);
+    
+    //If the button is pressed run this if statement
     if(digitalRead(buttons[i] = 0) == HIGH){
-      data[i+6]=1; //set the value of the array element to 1 for pressed (the +3 is to account for the start commands and joints)
+      
+      //set the value of the array element to 1 for pressed (the +6 is to account for the start commands and joints)
+      data[i+6]=1; 
+      
     }
   }
-
+  //Cycle throught all elements of the message and send
   for(int z = 0; z<11; z++){
-    Serial.write(data[z]); //Send the data array element by element (will be seen by reader as a series of integers)
+    
+    //Send the data array element by element (will be seen by reader as a series of integers)
+    Serial.write(data[z]); 
   }
 }
 
