6a1d2f9853c2f405daf83107ec260a125003e4aa
sir-kellsworth <kckswimmer@gmail.com>
2018-10-21 20:25:52 -0500

    got rid of some useless files that qt generated

--- Soro4/core/core.h
+++ Soro4/core/core.h
@@ -1,13 +1,13 @@
 #ifndef CORE_H
 #define CORE_H
 
-#include "core_global.h"
-
-class CORESHARED_EXPORT Core
-{
-
-public:
-    Core();
+/****
+Struct representing a cell of the topographic map used for autonomous pathfinding.
+*/
+struct Cell {
+	double lat;		//latitude of the cell
+	double lng;		//longitude of the cell
+	double gradient;	//average shade of the cell
 };
 
 #endif // CORE_H
