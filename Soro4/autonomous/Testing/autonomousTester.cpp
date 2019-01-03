35e8da86a0bfeecc77aac5908bfe29bea96d936f
RezaTorbati <kofcrotmgiv@gmail.com>
2019-01-03 13:04:07 -0600

    fixed an error with the test and have confirmed that inputNextCords and getWheelSpeedValues pass all tests

--- Soro4/autonomous/Testing/autonomousTester.cpp
+++ Soro4/autonomous/Testing/autonomousTester.cpp
@@ -2,33 +2,35 @@
 
 autonomousTester::autonomousTester()
 {
-    cout << "Wheel speed test: " << testGetWheelSpeedValues() << endl;
-    cout << "Input next cords test: " << testInputNextCords() << endl;
+    std::cout << "test wheel speed values result: " << testGetWheelSpeedValues() << std::endl;
+    std::cout << "test input next cords result: " << testInputNextCords() << std::endl;
+    std::cout << "test get angle to turn result: " << testGetAngleToTurn() << std::endl;
+    std::cout << "test update status result : " << testUpdateStatus() << std::endl;
 
 }
 
 int autonomousTester::testGetWheelSpeedValues()
 {
     //if the left wheel is turning the wrong way
-    if(getWheelSpeedValues(10, baseSpeed)[0] < baseSpeed)
+    if(getWheelSpeedValues(10, speed)[0] < speed)
         return -1;
 
     //if the right wheel is turning the wrong way
-    else if(getWheelSpeedValues(10, baseSpeed)[1] > baseSpeed)
+    else if(getWheelSpeedValues(10, speed)[1] > speed)
         return -2;
 
     //if the robot does not attempt to correct faster the further off it is
-    else if(getWheelSpeedValues(10, baseSpeed)[0] > getWheelSpeedValues(20, baseSpeed)[0])
+    else if(getWheelSpeedValues(10, speed)[0] > getWheelSpeedValues(20, speed)[0])
         return -3;
 
     //the below tests are the same as the above ones except instead check if the angle is negative
-    else if(getWheelSpeedValues(-10, baseSpeed)[1] < baseSpeed)
+    else if(getWheelSpeedValues(-10, speed)[1] < speed)
         return -4;
 
-    else if(getWheelSpeedValues(-10, baseSpeed)[0] > baseSpeed)
+    else if(getWheelSpeedValues(-10, speed)[0] > speed)
         return -5;
 
-    else if(getWheelSpeedValues(-10, baseSpeed)[1] > getWheelSpeedValues(-20, baseSpeed)[1])
+    else if(getWheelSpeedValues(-10, speed)[1] > getWheelSpeedValues(-20, speed)[1])
         return -6;
 
     return 0
