663562de05950752d6200abe36a8ae340c7a0eab
sir-kellsworth <kckswimmer@gmail.com>
2019-02-17 15:28:56 -0600

    switched arm code over to udp and changed pins

--- Arduino/arm_controller/arm_controller.ino
+++ Arduino/arm_controller/arm_controller.ino
@@ -1,164 +1,125 @@
 #include <Servo.h>
+#include <EtherCard.h>
+#include <IPAddress.h>
 
-#define BASE_PIN A0
-#define SHOULDER_PIN A5
-#define ELBOW_PIN 4
-#define CLAW_L_PIN 7
-#define CLAW_R_PIN 8
-#define ENC_L_PIN 11
-#define ENC_R_PIN 12
+enum DATA_POS : int{
+  START = 0,
+  ID,
+  DISABLE,
+  BASE,
+  SHOULDER,
+  ELBOW,
+  WRIST,
+  WRIST_ROTATE_SPEED,
+  CLAW,
+  HASH
+};
 
 const char DEVICE_ID = 1;
 
-// pin mapping
-Servo base, shoulder, elbow, clawL, clawR;
-// pin  3, 5, 6, 9, 10, and 11 are for analogWrite
-const int wristL1 = 5, wirstL2 = 6, wristR1 = 9, wristR2 = 10;
+// servos, pins and positions;
+Servo base;
+Servo shoulder;
+Servo elbow;
+Servo wristPitch;
+Servo wristRotate;
+Servo claw;
 
-// math constants
-const float ticksPerRev = 1669656;
-const float ticksRatio = 360.0 / ticksPerRev;
-const int wristTopSpeed = 127;// 255 max for analog write
-const int motorDeadZone = 1;
+//cannot use pins 13 - 11 and 8
+#define BASE_PIN 2
+#define SHOULDER_PIN 3
+#define ELBOW_PIN 4
+#define WRIST_PIN 5
+#define WRIST_ROTATE_PIN 6
+#define CLAW_PIN 7
+#define CLAW_STOPPER_PIN 18
 
-char myHash = 90;
-int elbow_pos = 135;
-int wristTheta_dest = 0;
-int wristPhi_dest = 0;
-int clawR_pos = 120;
-int clawL_pos = 58;
-int shoulder_pos = 135;
+//for analog reading pins
+#define BASE_ANALOG A0
+#define SHOULDER_ANALOG A1
+#define ELBOW_ANALOG A2
+#define WRIST_ANALOG A3
+
 int base_pos = 0;
-long lTicks = 0; // ticks since last update
-long rTicks = 0;
-bool rForward = true;
-bool lForward = true;
+int shoulder_pos = 135;
+int elbow_pos = 135;
+int wrist_pos = 0;
+//FIXME: eventually will get input from a joystick
+int wristRotateSpeed = 0;
+#define HOLD 90
+#define CLOCKWISE 120
+#define CCLOCKWISE 60
+//claw positions
+//also uses hold
+#define OPEN_CLAW 120
+#define CLOSE_CLAW 60
+int clawDir = HOLD;
 
-int wristTheta = 0;
-int wristPhi = 0;
-
-
-char incomingByte;
-int bytesRead = 0;
-
-char serResp[] = "1,190,200!!!!!!!!!!!!!!!!!!!!!!!!!\n";
-char serialHash;
-bool inTransmission = false;
+//Ethernet stuff
+static byte ip[] = {10,0,0,103};
+static byte gw[] = {10,0,0,1};
+static byte mac[] = {0x69,0xFF,0x3D,0x9A,0x88,0x12};
+byte Ethernet::buffer[500];
+int port = 1001;
+//messages
+char ack[] = "ack";
+char nack[] = "nack";
+char myHash = 90;
 
 //setup wheels
 void setup() {
   base.attach(BASE_PIN);
   shoulder.attach(SHOULDER_PIN);
   elbow.attach(ELBOW_PIN);
-  clawL.attach(CLAW_L_PIN);
-  clawR.attach(CLAW_R_PIN);
+  wristPitch.attach(WRIST_PIN);
+  wristRotate.attach(WRIST_ROTATE_PIN);
+
+  //set to default positions
   shoulder.write(shoulder_pos);
   elbow.write(elbow_pos);
-  clawL.write(clawL_pos);
-  clawR.write(clawR_pos);
+  wristPitch.write(wrist_pos);
+  wristRotate.write(wristRotateSpeed);
+  claw.write(clawDir);
 
-  attachInterrupt(digitalPinToInterrupt(ENC_L_PIN), leftEncoder, RISING);
-  attachInterrupt(digitalPinToInterrupt(ENC_R_PIN), rightEncoder, RISING);
   Serial.begin(9600);
+
+  //setup ethernet
+  // Change 'SS' to your Slave Select pin, if you aren't using the default pin
+  // Our ethernet card uses pin 10. SS is 8 by default. --Eric
+  if(ether.begin(sizeof Ethernet::buffer, mac, 10) == 0)
+    Serial.println("failed to access Ethernet controller");
+  ether.staticSetup(ip,gw);
+
+  ether.printIp("IP:  ", ether.myip);
+  ether.printIp("GW:  ", ether.gwip);
+  ether.printIp("DNS: ", ether.dnsip);
+
+  ether.udpServerListenOnPort(&handleMessage,port);
+  
   delay(10);
 }
 
-void loop() {
-  if (Serial.available() > 4) // trying to read from an empty buffer usually breaks stuff
-  { /*
-
-      incomingByte = Serial.read();
-      if(incomingByte == -127)
-      {
-      Serial.println("start flag");
-      if(Serial.read() != DEVICE_ID)
-      {
-       Serial.println("incorrect id");
-       // transmission is invalid, clear buffer and then reply with device id
-       Serial.flush();
-       Serial.write(-126);
-       Serial.write(DEVICE_ID);
+void handleMessage(uint16_t dest_port, uint8_t src_ip[IP_LEN], uint16_t src_port, const char *data, uint16_t len){
+    if(data[START] == -127){
+      base_pos = data[BASE];
+      shoulder_pos = data[SHOULDER];
+      elbow_pos = data[ELBOW];
+      wrist_pos = data[WRIST];
+      wristRotateSpeed = data[WRIST_ROTATE_SPEED];
+      clawDir = data[CLAW];
+      char hash = data[HASH];
+      myHash = (base_pos + shoulder_pos + elbow_pos/* + wrist_pos + claw_dir*/) / 3;//5;
+      if(hash == myHash){
+        ether.sendUdp(ack, sizeof(ack), src_port, src_ip, dest_port );
+        updateServos();
+      }else{
+        ether.sendUdp(nack, sizeof(nack), src_port, src_ip, dest_port );
       }
-      else{
-       Serial.println("reading message");
-       shoulder_pos = uint8_t(Serial.read());
-       elbow_pos = uint8_t(Serial.read());
-       clawL_pos = uint8_t(Serial.read());
-       clawR_pos = uint8_t(Serial.read());
-       serialHash = Serial.read();
-       myHash = (shoulder_pos + elbow_pos+clawL_pos+ clawR_pos)/4;
-       if(myHash == serialHash)
-       {
-         sprintf(serResp, "%d\t%d\t%d\t%d", shoulder_pos, elbow_pos, clawL_pos, clawR_pos);
-         Serial.println(serResp);
-         updateServos();
-       } else {
-         sprintf(serResp, "%d\t%d\t%d\t%d!!!!!!!", shoulder_pos, elbow_pos, clawL_pos, clawR_pos);
-         Serial.println(serResp);
-       }
-      }
-      }
-      }
-      else
-      {
-      //Serial.println("no bytes to read");
-      //Serial.read();// dispose packets untill the start flag(-127)
-      }
-
-      /**/
-    incomingByte = Serial.read();
-    if (incomingByte == -127 && !inTransmission) {
-      inTransmission = true;
-      bytesRead = 0;
-    } else if (inTransmission) {
-      switch (bytesRead) {
-        case 0: // device id
-          if (incomingByte != DEVICE_ID) {
-            // transmission is invalid, clear buffer and then reply with device id
-            Serial.flush();
-            Serial.write(-126);
-            Serial.write(DEVICE_ID);
-            inTransmission = false;
-          }
-          break;
-        case 1:
-          base_pos = uint8_t(incomingByte);
-          break;
-        case 2: // shoulder
-          shoulder_pos = uint8_t(incomingByte);
-          break;
-        case 3: // elbow
-          elbow_pos = uint8_t(incomingByte);
-          break;
-        case 4:
-          wristTheta_dest = int8_t(incomingByte);// converts from int to float. change the transmission
-          break;
-        case 5:
-          wristPhi_dest = int8_t( incomingByte);// TODO change the transmission to be a float for precision
-          break;
-        case 6:
-          clawL_pos = uint8_t(incomingByte);
-          break;
-        case 7:
-          clawR_pos = uint8_t(incomingByte);
-          break;
-        case 8: // hash (last byte recieved in transmission)
-          serialHash = incomingByte;
-          inTransmission = false;
-          myHash = (base_pos + shoulder_pos + elbow_pos + wristTheta_dest + (wristPhi_dest) + clawL_pos + clawR_pos) / 7;
-          if (myHash == serialHash) {
-            sprintf(serResp, "%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d", base_pos, shoulder_pos, elbow_pos, (int8_t)wristTheta_dest, (int8_t)wristPhi_dest, clawL_pos, clawR_pos, myHash);
-            Serial.println(serResp);
-            updateServos();
-          } else {
-            sprintf(serResp, "%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d!!!!!!!", base_pos, shoulder_pos, elbow_pos, (int8_t)wristTheta_dest, (int8_t)wristPhi_dest, clawL_pos, clawR_pos, myHash);
-            Serial.println(serResp);
-          }
-          break;
-      }
-      bytesRead++;
     }
-  }/**/
+}
+
+void loop() {
+  ether.packetLoop(ether.packetReceive());
 }
 
 void updateServos() {
@@ -166,69 +127,12 @@ void updateServos() {
   base.write(base_pos);
   shoulder.write(shoulder_pos);
   elbow.write(elbow_pos);
-  clawL.write(clawL_pos);
-  clawR.write(clawR_pos);
-
-  // Right now we can't rotate and tilt ar the same time we'll need to fix this later
-  if (wristPhi_dest > 0) {
-    // rotate right
-    //Serial.println(wristPhi_dest);
-    analogWrite(5, 200);//wristPhi_dest << 1);
-    analogWrite(6, 0);
-    analogWrite(3, 0);
-    analogWrite(11, 200);//wristPhi_dest << 1);
-    return;
-  }
-  if (wristPhi_dest < 0) {
-
-    analogWrite(5, 0);
-    analogWrite(6, 200);//abs(wristPhi_dest)*2);
-    analogWrite(3, 200);//abs(wristPhi_dest)*2);
-    analogWrite(11, 0);
-    return;
-  }
-  if (wristTheta_dest > 0) {
-    // rotate right
-    analogWrite(5, 200);//wristTheta_dest << 1);
-    analogWrite(6, 0);
-    analogWrite(3, 200);//wristTheta_dest << 1);
-    analogWrite(11, 0);
-    return;
-  }
-  if (wristTheta_dest < 0) {
-    // rotate right
-    analogWrite(5, 0);
-    analogWrite(6, 200);//abs(wristTheta_dest)*2);
-    analogWrite(3, 0);
-    analogWrite(11, 200);//abs(wristTheta_dest)*2);
-    return;
-  }
-  if (wristPhi_dest == 0 && wristTheta_dest == 0) {
-    analogWrite(5, 0);
-    analogWrite(6, 0);
-    analogWrite(3, 0);
-    analogWrite(11, 0);
-  }
-
-
-}
-/*
-   this interuptes the processor to count the number of pulses that come from the encoder.
-
-   Warning this can happen as much as 1,669,656 times a second, and the arduino clock is only 16mhz
-*/
-void leftEncoder() {
-  lTicks++;
-}
-/*
-   same as left encoder
-*/
-void rightEncoder() {
-  rTicks++;
+  wristPitch.write(wrist_pos);
+  wristRotate.write(wristRotateSpeed);
+  claw.write(clawDir);
 }
 
 void calibrate() {
-
 
 }
 
--- Arduino/masterArmReader/masterArmReader.ino
+++ Arduino/masterArmReader/masterArmReader.ino
@@ -32,6 +32,9 @@
  *  will return an integer 2**n smaller than the original integer if the bit is 0. If the output of this XOR is the same number then that bit is not a 1.
  */
 
+#include <EtherCard.h>
+#include <IPAddress.h>
+
 //Serial Communication ID so the computer knows what device this is.
 int ID = 1; 
 
@@ -41,12 +44,53 @@ int ID = 1;
 int buttons[4] = {0,1,2,3}; //rollLeft (CCW from the rover's perspective), rollRight (CW from the rover's perspective), let go, grab (digital ports connected to buttons)
 int joints[4] = {0,1,2,3}; //yaw, shoulder, elbow, wrist pitch (the analog ports the potentiometers are connected to)
 
+//Ethernet stuff
+static byte ip[] = {10,0,0,105};
+static byte gw[] = {10,0,0,1};
+static byte mac[] = {0x69,0xFF,0x3D,0x9A,0x88,0x12};
+static byte roverIP[] = {10,0,0,103};
+byte Ethernet::buffer[500];
+uint8_t port = 5505;
+uint8_t slavePort= 1001;
+
+//cannot use pins 13 - 11 and 8
+//for digital control pins
+#define CLOSE_CLAW_PIN 2
+#define OPEN_CLAW_PIN 3
+#define STOW_ARM 4
+#define DISABLE_ARM 5
+
+//for analog reading pins
+#define BASE_ANALOG A0
+#define SHOULDER_ANALOG A1
+#define ELBOW_ANALOG A2
+#define WRIST_ANALOG A3
+#define JOYSTICK A4
+
+void handleMessage(uint16_t dest_port, uint8_t src_ip[IP_LEN], uint16_t src_port, const char *data, uint16_t len){
+
+}
+
 void setup() {
   //Start talking to the computer
   Serial.begin(9600);
+
+  //setup ethernet
+  // Change 'SS' to your Slave Select pin, if you aren't using the default pin
+  // Our ethernet card uses pin 10. SS is 8 by default. --Eric
+  if(ether.begin(sizeof Ethernet::buffer, mac, 10) == 0)
+    Serial.println("failed to access Ethernet controller");
+  ether.staticSetup(ip,gw);
+
+  ether.printIp("IP:  ", ether.myip);
+  ether.printIp("GW:  ", ether.gwip);
+  ether.printIp("DNS: ", ether.dnsip);
+
+  ether.udpServerListenOnPort(&handleMessage,port);
 }
 
 void loop() {
+  ether.packetLoop(ether.packetReceive());
   
   //this doesn't have to be in a seperate function I did this in case we want to add stuff not taking data later.
   sendData(); 
@@ -58,13 +102,14 @@ void sendData(){
   0.263671875 is the value 270°/1024 to convert the potentiometer output to degrees (5v/1024 represents 270°)
   NOTE: The converstion to a int restricts our accuracy to a minimum of ~4° of accuracy and to type on a keyboard we need 1.2°)
   */
-  int data[7];
+  //FIXME: may need to change this to char
+  char data[8];
   data[0] = -127; //Start command to tell the computer we are sending data
   data[1] = ID; //ID number to identify the arduino this is coming from.
-  data[2] = (int)analogRead(joints[0])*0.263671875; //Get the data from the Yaw Pot. (rotate arm horizontally)
-  data[3] = (int)analogRead(joints[1])*0.263671875; //Get the data from the Shoulder Pot. (the bottom joint)
-  data[4] = (int)analogRead(joints[2])*0.263671875; //Get the data from the Elbow Pot. (the second from the bottom joint)
-  data[5] = (int)analogRead(joints[3])*0.263671875; //Get the data from the Wrist Pitch Pot. (rotate the finger/hand pitch)
+  data[2] = (char)analogRead(joints[0])/1024.0;//*0.263671875; //Get the data from the Yaw Pot. (rotate arm horizontally)
+  data[3] = (char)analogRead(joints[1])/1024.0;//*0.263671875; //Get the data from the Shoulder Pot. (the bottom joint)
+  data[4] = (char)analogRead(joints[2])/1024.0;//*0.263671875; //Get the data from the Elbow Pot. (the second from the bottom joint)
+  data[5] = (char)analogRead(joints[3])/1024.0;//*0.263671875; //Get the data from the Wrist Pitch Pot. (rotate the finger/hand pitch)
   data[6] = 0; //Set all the button data to default to 0 (low)
   
 
@@ -94,10 +139,15 @@ void sendData(){
   for(int z = 0; z<7; z++){
     
     //Send the data array element by element (will be seen by reader as a series of integers)
-    Serial.write(data[z]);
+    Serial.print(data[z]);
+    Serial.print(" ");
+
     
     //Increase the sum by the data value so we can average it out at the end
     sum = sum + data[z];
   }
-  Serial.write(sum/7);
+  Serial.println(sum/7);
+  data[7] = sum / 7;
+  ether.sendUdp(data, sizeof(data), port, roverIP, slavePort);
+
 }
