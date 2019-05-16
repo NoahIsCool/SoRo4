2b2fbfad168d80048877e91f23ec7736f719fe65
sir-kellsworth <kckswimmer@gmail.com>
2019-05-16 12:10:54 -0600

    added simple arm controller

---
+++ ArmController/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <QCoreApplication>
+
+#include "gamepadmonitor.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    GamepadMonitor monitor;
+    monitor.init();
+
+    return a.exec();
+}
